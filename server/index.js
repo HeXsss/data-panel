64ee7de0d71e59fc139e06367d07f1d7a82c8bf0
HeXsss <officialohexs@gmail.com>
2022-02-14 17:39:19 +0100

    Added mysql connection via .env

--- server/index.js
+++ server/index.js
@@ -8,6 +8,23 @@ app.use(cors());
 const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
+const mysql = require('mysql');
+const connectionData = {
+  host: process.env.HOST || 'localhost',
+  user: process.env.USER || 'root',
+  password: process.env.PASSWORD || '',
+  database: process.env.DATABASE || 'roleplay',
+}
+const connection = mysql.createConnection(connectionData)
+try {
+  connection.connect();
+} catch (err) {
+  console.log(err)
+}
+// connection.query('SELECT COUNT(*) FROM societies', (error, results, fields) => {
+//   if (error) throw error;
+//   console.log(results[0]['COUNT(*)'])
+// })
 app.listen(port, (req, res, next) => {
   console.log(`🚀 Server booted on port ${port}`)
 })
